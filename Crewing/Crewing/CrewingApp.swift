8a8fb402fc262cb8b431464f8457b433188c69fd
ImKyungJin <dlarudwls098@gmail.com>
2024-04-30 01:38:16 +0900

    [setting] 프로젝트 파일 생성

---
+++ Crewing/Crewing/CrewingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewingApp.swift
+//  Crewing
+//
+//  Created by 임경진 on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CrewingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
