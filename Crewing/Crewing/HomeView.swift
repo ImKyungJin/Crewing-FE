af366fb945973d5bb88c46761e5159e1bf8212a5
ImKyungJin <dlarudwls098@gmail.com>
2024-05-13 00:36:36 +0900

    하단탭 및 탭 별 파일 생성

---
+++ Crewing/Crewing/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/13/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Crewing/Crewing/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/13/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
