1560a35bce50e446c0469ec80ea905c49c6bff55
ImKyungJin <dlarudwls098@gmail.com>
2024-05-13 00:26:49 +0900

    하단탭, 탭view 파일 생성

---
+++ Crewing/Crewing/ClubView.swift
@@ -0,0 +1,18 @@
+//
+//  ClubView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ClubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubView()
+}
---
+++ Crewing/Crewing/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
