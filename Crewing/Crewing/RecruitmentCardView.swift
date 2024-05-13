48eef2c36e7e01bab49801f8785427541872c6bc
ImKyungJin <dlarudwls098@gmail.com>
2024-05-13 16:51:09 +0900

    홈탭 첫화면 구성 완료

---
+++ Crewing/Crewing/RecruitmentCardView.swift
@@ -0,0 +1,18 @@
+//
+//  RecruitmentCardView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/13/24.
+//
+
+import SwiftUI
+
+struct RecruitmentCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecruitmentCardView()
+}
