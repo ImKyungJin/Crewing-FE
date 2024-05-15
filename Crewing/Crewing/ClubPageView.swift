82200f9fda392a615245a3b1a2dfaf22948f7e77
ImKyungJin <dlarudwls098@gmail.com>
2024-05-15 19:11:16 +0900

    동아리 페이지 화면 구성

---
+++ Crewing/Crewing/ClubPageView.swift
@@ -0,0 +1,18 @@
+//
+//  ClubPageView.swift
+//  Crewing
+//
+//  Created by 임경진 on 5/15/24.
+//
+
+import SwiftUI
+
+struct ClubPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClubPageView()
+}
